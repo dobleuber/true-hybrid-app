bb53265950efe6ee3068575a6ac12bf01f6f795c
Wbert Adrian Castro Vera <wcastrov@WcastroV.local>
2015-09-10 14:42:44 -0500

    Added HLS video and an image over the video

--- MyNativeCordovaExample/Classes/VideoViewController.m
+++ MyNativeCordovaExample/Classes/VideoViewController.m
@@ -12,8 +12,13 @@
 @synthesize streamPlayer = _streamPlayer;
 
 - (void)viewDidLoad {
-    NSURL *streamURL = [NSURL URLWithString:@"http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4"];
+    NSURL *streamURL = [NSURL URLWithString:@"http://104.238.118.147:1935/vod/mp4:sample.mp4/playlist.m3u8"];
+    UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon-50@2x.png"]];
+    image.frame = CGRectMake(self.view.frame.size.width / 4, self.view.frame.size.height / 4, self.view.frame.size.width / 2, self.view.frame.size.height / 2);
+
     _streamPlayer = [[MPMoviePlayerController alloc] initWithContentURL:streamURL];
+    [self.streamPlayer.view addSubview:image];
+    [self.streamPlayer prepareToPlay];
     [self.streamPlayer.view setFrame: self.view.bounds ];
     self.streamPlayer.controlStyle = MPMovieControlStyleEmbedded;
     [self.view addSubview: self.streamPlayer.view];
